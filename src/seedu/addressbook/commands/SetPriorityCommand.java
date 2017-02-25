54d9610dc6612f823f078fbdc9ea83db9231e2f6
mouziyanglovestudy <e0012656@u.nus.edu>
2017-02-25 16:05:59 +0800

    Add SetPriorityCommand class

---
+++ src/seedu/addressbook/commands/SetPriorityCommand.java
@@ -0,0 +1,43 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
+
+public class SetPriorityCommand extends Command{
+    
+    public static final String COMMAND_WORD = "reprioritize";
+    
+    public static final String MESSAGE_INVALID_PRIORITIZE_LEVEL = "The prioritize level should between 1 to 3.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
+            + "Repriorize the person identified by the index number used in the last person listing.\n\t"
+            + "Parameters: INDEX NEW_PRIORITIZE_LEVEL\n\t"
+            + "Example: " + COMMAND_WORD + " 1" + " 3";
+
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Repriorize Person: %1$s";
+
+    private int newLevel;
+
+    public SetPriorityCommand(int targetVisibleIndex, int newLevel) {
+        super(targetVisibleIndex);
+        this.newLevel = newLevel;
+    }
+
+    @Override
+    public CommandResult execute() {
+        try {
+            final ReadOnlyPerson target = getTargetPerson();
+            addressBook.repriorize(target, this.newLevel);
+            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, target));
+
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (IllegalValueException e) {
+            return new CommandResult(MESSAGE_INVALID_PRIORITIZE_LEVEL);
+        }
+    }
+    
+    
+}
